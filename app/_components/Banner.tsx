23df97c0e748ef572f3eefdac08152b8b7f5bb20
tanishq0917t <tanishqrawat8@gmail.com>
2025-08-20 13:46:31 +0530

    Testing deployment

--- app/_components/Banner.tsx
+++ app/_components/Banner.tsx
@@ -58,7 +58,7 @@ const Banner = () => {
                         as="link"
                         target="_blank"
                         rel="noopener noreferrer"
-                        href={GENERAL_INFO.upworkProfile}
+                        href=""
                         variant="primary"
                         className="mt-9 banner-button slide-up-and-fade"
                     >
